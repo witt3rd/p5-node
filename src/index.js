8159ece94e112afe4b5f919191277a110223b3d5
witt3rd <witt3rd@gmail.com>
2018-06-16 11:06:52 -0700

    mucked with linting and formatting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import express from "express";
+import express from 'express';
+
 const app = express();
 
 const PORT = 8081;
 
-app.use(express.static("public"));
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`p5-test listening on port ${PORT}`));
+const x = {};
+
+app.listen(PORT, () => console.log('p5-test listening on port ${PORT}'));
